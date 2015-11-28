43d5b4e0228791c46392b84108d9f4eef48d7e67
Michael Lutaaya <michael@lutaaya.com>
2015-11-27 22:28:40 -0500

    Write functions for sending + receiving meetings

--- networking.h
+++ networking.h
@@ -11,8 +11,12 @@
 #include <string>
 using namespace std;
 
+class Meeting;
+
 int sendMessage(string& buff, int descriptor);
 int receiveMessage(string& buff, int descriptor);
 void setupListenSocket(int port, int *listen_socket);
 void acceptIncomingConnection(int *listen_socket, int *accept_socket);
 void connectToServer(char *ip_address, int port, int *descriptor);
+void sendMeeting(Meeting& meeting, int descriptor);
+void receiveMeeting(Meeting& meeting, int descriptor);
