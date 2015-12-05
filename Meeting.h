1631eada9d6cc8612106ef7ac679dcb7c6568f2b
Michael Lutaaya <michael@lutaaya.com>
2015-12-05 11:46:59 -0500

    Remove extra function declaration

--- Meeting.h
+++ Meeting.h
@@ -28,7 +28,6 @@ public:
   string possible_times_as_string() const;
   string topic_as_string_no_asterisks(string&);
   string topic_as_string_asterisks() const;
-  string topic_as_string_noastericks();
   icalcomponent * to_icalcomponent();
   friend ostream& operator<<(ostream& out, const Meeting& obj);
   friend istream& operator>>(istream& in, Meeting& obj);
